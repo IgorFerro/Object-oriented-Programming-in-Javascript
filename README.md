9a942c7eda6c9af228c25c6d88e8a5082bd21dc0
Igor Ferro Parreira <ifparreira@uolinc.com>
2019-12-05 07:42:20 -0300

    Object Literals

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Object-oriented-Programming-in-Javascript
 Object-oriented Programming in Javascript
+
+#Install Live Server
+1- Install Live Server from extensions
+
---
+++ index.js
@@ -0,0 +1,14 @@
+//Object Literals
+
+const circle = {
+    radius: 1,
+    location: {
+        x: 1,
+        y: 1,
+    },
+    draw: function(){
+        console.log('draw');
+    }
+};
+
+circle.draw();
