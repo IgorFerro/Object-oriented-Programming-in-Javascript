21610ded06f16d2dee5950e8b3b957c4ea07c784
Igor Ferro Parreira <ifparreira@uolinc.com>
2019-12-06 07:12:06 -0300

    Adding or Removing Properties

--- index.js
+++ index.js
@@ -53,7 +53,7 @@ null*/
 
 // Primitives are copied by their value
 //Objects are copied by their reference
-
+/*
 let obj = {value: 10};
 
 function increase(obj){
@@ -62,3 +62,39 @@ function increase(obj){
 
 increase(obj);
 console.log(obj);
+*/
+///////////////////////////////////////////////////////////
+//Adding or Removing Properties
+//Constroctur Function
+function Circle(radius){
+    this.radius= radius;
+    this.draw= function(){
+        console.log('draw');
+    }
+}
+
+//Object
+const circle = new Circle(10);
+
+//Create New Object
+circle.location = {x:1};
+
+//Add new object 
+const propertyName = 'center location';
+circle.propertyName = {x:1};
+
+//Delete a property
+delete circle['location'];
+
+/////////////////////////////////////////////////////////////
+//Enumerating Properties
+//Constroctur Function
+function Circle(radius){
+    this.radius= radius;
+    this.draw= function(){
+        console.log('draw');
+    }
+}
+
+//Object
+const circle = new Circle(10);
